ced5a72585626f72a7baf59efd8df0c73bb4e93e
Artem Lukianets <mail.lukianets@gmail.com>
2020-06-11 18:45:38 +0300

    Added nodemon & morgan modules

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -16,7 +16,7 @@ router.post('/', (req, res, next) => {
 router.get('/:orderId', (req, res, next) => {
     res.status(200).json({
         message: 'Orders details',
-        orderId: req.params.orderId
+        orderId: req.params.orderId,
     })
 })
 
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 const productRoutes = require('./api/routes/products')
