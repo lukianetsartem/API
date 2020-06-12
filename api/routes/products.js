056d7917feeb018ddbb3c9aed4ba30c40bad1bdb
Artem Lukianets <mail.lukianets@gmail.com>
2020-06-12 19:49:45 +0300

    Improved product requests

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,12 +6,24 @@ const Product = require('../models/product')
 
 router.get('/', (req, res, next) => {
     Product.find()
+        .select('name price _id')
         .exec()
-        .then(docs => {
-            docs.length >= 0 ? res.status(200).json(docs)
-                : res.status(404).json({
-                    message: 'No entries found'
+        .then(result => {
+            const response = {
+                count: result.length,
+                products: result.map(result => {
+                    return {
+                        name: result.name,
+                        price: result.price,
+                        _id: result._id,
+                        request: {
+                            type: 'GET',
+                            url: `http://localhost:3000/products/${result._id}`
+                        }
+                    }
                 })
+            }
+            res.status(200).json(response)
         })
         .catch(err => {
             res.status(500).json({
@@ -29,7 +41,15 @@ router.post('/', (req, res, next) => {
     product.save()
         .then(result => {
             res.status(201).json({
-                createdProduct: result
+                createdProduct: {
+                    name: result.name,
+                    price: result.price,
+                    _id: result._id,
+                    request: {
+                        type: 'GET',
+                        url: `http://localhost:3000/products/${result._id}`
+                    }
+                }
             })
         })
         .catch(err => {
@@ -42,9 +62,16 @@ router.post('/', (req, res, next) => {
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId
     Product.findById(id)
+        .select('name price _id')
         .exec()
-        .then(doc =>{
-            doc ? res.status(200).json(doc)
+        .then(result => {
+            result ? res.status(200).json({
+                    product: result,
+                    request: {
+                        type: 'GET',
+                        url: `http://localost:3000/products/${result._id}`
+                    }
+                })
                 : res.status(404).json({
                     message: 'No valid entry found for provided ID'
                 })
@@ -58,18 +85,25 @@ router.get('/:productId', (req, res, next) => {
 
 router.patch('/:productId', (req, res, next) => {
     const id = req.params.productId
-    const updateOps = {}
-    for (const ops of req.body) {
-        updateOps[ops.propertyName] = ops.value
+    const updateOps = {} // Object with new product properties
+    for (const ops of req.body) { // Iterate through array of product properties
+        updateOps[ops.propertyName] = ops.value // propertyName - name of product property, that you want to edit, value - him value
     }
     Product.update(
-        { _id: id },
-        { $set: updateOps})
+        {_id: id},
+        {$set: updateOps})
         .exec()
         .then(result => {
-            res.status(200).json(result)
+            res.status(200).json({
+                product: result,
+                message: 'Product has been updated',
+                request: {
+                    type: 'PATCH',
+                    url: `http://localost:3000/products/${result._id}`
+                }
+            })
         }).catch(err => {
-            res.status(500).json({
+        res.status(500).json({
             error: err
         })
     })
@@ -77,10 +111,16 @@ router.patch('/:productId', (req, res, next) => {
 
 router.delete('/:productId', (req, res, next) => {
     const id = req.params.productId
-    Product.remove({ _id: id })
+    Product.remove({_id: id})
         .exec()
         .then(result => {
-            res.status(200).json(result)
+            res.status(200).json({
+                message: 'Product has been deleted',
+                request: {
+                    type: 'DELETE',
+                    url: `http://localost:3000/products/${result._id}`
+                }
+            })
         })
         .catch(err => {
             res.status(500).json({
