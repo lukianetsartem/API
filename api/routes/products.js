c8e10ebbc8f29553ed730bca1af19d050ff95b5a
Artem Lukianets <mail.lukianets@gmail.com>
2020-06-12 18:48:30 +0300

    Connected to MongoDB, added mongoose, big changes in products requests

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,33 +1,92 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+
+const Product = require('../models/product')
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'get products'
-    })
+    Product.find()
+        .exec()
+        .then(docs => {
+            docs.length >= 0 ? res.status(200).json(docs)
+                : res.status(404).json({
+                    message: 'No entries found'
+                })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
 })
 
 router.post('/', (req, res, next) => {
-    const product = {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
-        price: req.body.price
-    };
-    res.status(201).json({
-        message: 'Handling POST requests to /products',
-        createdProduct: product
-    });
-});
+        price: req.body.price,
+    })
+    product.save()
+        .then(result => {
+            res.status(201).json({
+                createdProduct: result
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
+})
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId
+    Product.findById(id)
+        .exec()
+        .then(doc =>{
+            doc ? res.status(200).json(doc)
+                : res.status(404).json({
+                    message: 'No valid entry found for provided ID'
+                })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
+})
 
 router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Updated product!'
+    const id = req.params.productId
+    const updateOps = {}
+    for (const ops of req.body) {
+        updateOps[ops.propertyName] = ops.value
+    }
+    Product.update(
+        { _id: id },
+        { $set: updateOps})
+        .exec()
+        .then(result => {
+            res.status(200).json(result)
+        }).catch(err => {
+            res.status(500).json({
+            error: err
+        })
     })
 })
 
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted product!'
-    })
+    const id = req.params.productId
+    Product.remove({ _id: id })
+        .exec()
+        .then(result => {
+            res.status(200).json(result)
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
 })
 
 module.exports = router
--- app.js
+++ app.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
+
+mongoose.connect(`mongodb+srv://admin:J4aI2d@storedb-onq18.mongodb.net/storeDB?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+)
 
 app.use(morgan("dev"))
 app.use(bodyParser.urlencoded({ extended: false }))
